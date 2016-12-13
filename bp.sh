a4d7432b12a32d478c5dfc5cc05f7ca0e033e66c
aklliy.mou <aklliy.mou@zeusis.com>
2016-12-13 13:57:20 +0800

    ROM-4713

---
+++ bp.sh
@@ -0,0 +1,5 @@
+npm run build
+rm -rf D:/works/3dnp-web/node_modules/draft-js/dist/
+rm -rf D:/works/3dnp-web/node_modules/draft-js/lib/
+cp -rf dist D:/works/3dnp-web/node_modules/draft-js/
+cp -rf lib  D:/works/3dnp-web/node_modules/draft-js/
